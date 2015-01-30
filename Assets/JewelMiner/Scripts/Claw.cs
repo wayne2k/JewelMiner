74e32407a09eb190ce1f40958c0d7971735deee0
wayne2k <wayne2k@gmail.com>
2015-01-30 15:17:46 +0200

    Basic Game Working
    Gun and claw scripts added.
    Basic functionality tested: working

---
+++ Assets/JewelMiner/Scripts/Claw.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.JewelMiner
+{
+	public class Claw : MonoBehaviour 
+	{
+		public Transform origin;
+		public float speed = 4f;
+		public Gun gun;
+
+		Vector3 _target;
+		int _jewelValue = 100;
+		GameObject _childObject;
+		LineRenderer _lineRenderer;
+		bool _hitJewel;
+		bool _retracting;
+
+		void Awake ()
+		{
+			_lineRenderer = GetComponent <LineRenderer> ();
+		}
+
+		void Update ()
+		{
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, _target, step);
+
+			_lineRenderer.SetPosition (0, origin.position);
+			_lineRenderer.SetPosition (1, transform.position);
+
+			if (transform.position == origin.position && _retracting)
+			{
+				gun.CollectedObject ();
+				if (_hitJewel)
+				{
+					_hitJewel = false;
+				}
+				Destroy (_childObject);
+				gameObject.SetActive (false);
+			}
+		}
+
+		void OnTriggerEnter (Collider other)
+		{
+			_retracting = true;
+			_target = origin.position;
+
+			if (other.gameObject.CompareTag ("Jewel")) 
+			{
+				_hitJewel = true;
+				_childObject = other.gameObject;
+				other.transform.SetParent (this.transform);
+			}
+			else if (other.gameObject.CompareTag ("Rock"))
+			{
+				_childObject = other.gameObject;
+				other.transform.SetParent (this.transform);
+			}
+		}
+
+		public void ClawTarget (Vector3 pos)
+		{
+			_target = pos;
+		}
+	}
+}
---
+++ Assets/JewelMiner/Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.JewelMiner
+{
+	public class Gun : MonoBehaviour 
+	{
+		public GameObject claw;
+		public bool isShooting;
+		public Animator _minerAnim;
+		public Claw clawScript;
+
+		void Update () 
+		{
+			if (Input.GetButtonDown ("Fire1") && isShooting == false) 
+			{
+				LaunchClaw ();			
+			}
+		}
+
+		void LaunchClaw ()
+		{
+			isShooting = true;
+			_minerAnim.speed = 0f;
+			RaycastHit hit;
+			Vector3 down = transform.TransformDirection (Vector3.down);
+
+			if (Physics.Raycast (transform.position, down, out hit, 100f))
+			{
+				claw.SetActive (true);
+				clawScript.ClawTarget (hit.point);
+			}
+		}
+
+		public void CollectedObject ()
+		{
+			isShooting = false;
+			_minerAnim.speed = 1f;
+		}
+	}
+}
