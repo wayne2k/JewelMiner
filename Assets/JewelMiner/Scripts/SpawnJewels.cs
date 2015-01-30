bfefe7f4b53a6e6b107f9d83974315caba9ea811
wayne2k <wayne2k@gmail.com>
2015-01-30 16:09:45 +0200

    Completed Project
    Added:
    Spawn rocks jewels
    
    Fixed Line renderer.

---
+++ Assets/JewelMiner/Scripts/SpawnJewels.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.JewelMiner
+{
+	public class SpawnJewels : MonoBehaviour 
+	{
+		public int xRange = 10;
+		public int yRange = 3;
+		public int numObjects = 10;
+
+		public GameObject[] objects;
+
+		void Start ()
+		{
+			Spawn ();		
+		}
+
+		void Spawn ()
+		{
+			for (int i=0; i < numObjects; i++)
+			{
+				Vector3 spawnLoc = new Vector3 (Random.Range (-xRange, xRange), Random.Range (-yRange, yRange), 0f);
+				int objectPick = Random.Range (0, objects.Length);
+
+				Instantiate (objects[objectPick], spawnLoc, Random.rotation);
+			}
+		}
+	}
+}
